66c06d8ca9234eb13a049eae5f1eeaef67d4304b
alishadnaidu <alishanaidu@fb.com>
2021-07-07 14:50:32 -0700

    Define Data Model

--- app/src/main/java/com/example/parstagram/ParseApplication.java
+++ app/src/main/java/com/example/parstagram/ParseApplication.java
@@ -3,11 +3,15 @@ package com.example.parstagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        //register parse models
+        ParseObject.registerSubclass(Post.class);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("kBiR9O22JABC9QPcFhsnSJcFCCtjctmfQlZ600K4")
